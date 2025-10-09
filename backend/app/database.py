354b62c3f81b90946b201da98e57a3e884f37390
rmdOG <ryan.davies.oilgas@gmail.com>
2025-10-09 16:29:32 -0600

    fixing error starting backend.

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Must be before getenv
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
